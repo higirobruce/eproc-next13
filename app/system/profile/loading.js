78937dd40197292975d401de81a9a323866ba609
higirobruce <higirobru@gmail.com>
2023-05-15 15:36:58 +0200

    Update loading.js

--- app/system/profile/loading.js
+++ app/system/profile/loading.js
@@ -1,9 +1,11 @@
+
 import React from 'react'
+import Spinner from '../../components/spinner'
 
 export default function loading() {
   return (
-    <div>
-      loading...
-    </div>
+    <div className="flex flex-col items-center justify-center h-screen">
+    <Spinner/>
+  </div>
   )
 }
