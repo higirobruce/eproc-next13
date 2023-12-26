1fb0f9134d4f8e561890da4c9682c97578afee7d
Bruce Higiro <higirobru@gmail.com>
2023-12-26 21:55:47 +0200

    Update page.js

--- app/system/payment-requests/[id]/page.js
+++ app/system/payment-requests/[id]/page.js
@@ -1901,6 +1901,7 @@ export default function PaymentRequest({ params }) {
                       );
                     })}
                   </div>
+                  
                 </div>
               )}
             </>
