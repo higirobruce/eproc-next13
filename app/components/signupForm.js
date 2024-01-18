0a114e20317e362446c21910a26670a278e03c10
Bruce Higiro <higirobru@gmail.com>
2024-01-18 22:59:50 +0200

    Update signupForm.js

--- app/components/signupForm.js
+++ app/components/signupForm.js
@@ -2040,14 +2040,24 @@ const SignupForm = () => {
                       <div className="grid grid-cols-2 gap-x-5">
                         <div>
                           <div>Hq Address</div>
-                          <Form.Item name="hqAddress">
+                          <Form.Item name="hqAddress" rules={[
+                              {
+                                required: true,
+                                message: "Input required",
+                              },
+                            ]}>
                             <Input />
                           </Form.Item>
                         </div>
 
                         <div>
                           <div>Country</div>
-                          <Form.Item name="country">
+                          <Form.Item name="country" rules={[
+                              {
+                                required: true,
+                                message: "Input required",
+                              },
+                            ]}>
                             <Input />
                           </Form.Item>
                         </div>
